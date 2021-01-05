6267940213e2ba8fcaba8b8ccbde38642507c30f
yunyeojun1234 <pp1ii2gg3@naver.com>
2021-01-06 00:21:00 +0900

    첫 commit

---
+++ prepare/front/components/AppLayout.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Link from 'next/link';    //자체적인 라우터 이다. 외우자 링크 컴포넌트,
+
+
+/* *******children이 무엇을 뜻하는지 공부******** */
+//화면안에 그릴수 있는 모든 것들이 노드다
+const AppLayout= ({children}) => {
+    return (
+    <div>
+        <div>
+            <Link href="/"><a>노드버드</a></Link>
+            <Link href="/profile"><a>프로필</a></Link>
+            <Link href="/signup"><a>회원가입</a></Link>
+        </div>
+        {children}
+    </div>
+    );
+};
+
+AppLayout.propTypes ={
+    children: PropTypes.node.isRequired,
+};
+
+export default AppLayout;
+/* 코드점검툴 */
+//npm i eslint -D
+//npm i eslint-plugin-import -D
+// npm i eslint-plugin-react -D   
+//npm i eslint-plugin-react-hooks -D   
---
+++ prepare/front/pages/profile.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import AppLayout from '../components/AppLayout';
+
+const Profile =()=> {
+    return <AppLayout>프로필</AppLayout>
+};
+
+export default Profile;
